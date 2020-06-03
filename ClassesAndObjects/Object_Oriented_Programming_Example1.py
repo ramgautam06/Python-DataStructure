e7c27b9839abb28f6a826527fc8a12cb72f3e055
rgautam <rgautam@aclara.com>
2020-06-03 16:17:00 -0400

    Object Oriented Programming - Example 1 - adding two numbers

--- ClassesAndObjects/Object_Oriented_Programming_Example1.py
+++ ClassesAndObjects/Object_Oriented_Programming_Example1.py
@@ -3,5 +3,24 @@
 Created on Wed Jun  3 15:56:16 2020
 
 @author: rgautam
+
+This class prints the sum of two numbers
+
 """
 
+class example1:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2 
+        
+    def sum(self):
+        total = self.num1 + self.num2
+        return total 
+    
+def main():
+    print("Starting Class")
+    exe = example1(10 , 7) #exe is an object 
+    print("The sum of numbers is " + str(exe.sum()))
+    
+if __name__ == '__main__':
+    main()
